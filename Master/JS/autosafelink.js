5338d364ae5c40bc6b78bbcb91a99e51140b2cb5
Angga Saputra <masalikzxc@gmail.com>
2017-07-03 23:34:01 +0700

    Update autosafelink.js

--- Master/JS/autosafelink.js
+++ Master/JS/autosafelink.js
@@ -1,1 +1,33 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5 Q={o:"1v+/=",V:D(8){5 b="";5 C,t,q,H,w,k,l;5 i=0;8=Q.1e(8);T(i<8.F){C=8.p(i++);t=8.p(i++);q=8.p(i++);H=C>>2;w=((C&3)<<4)|(t>>4);k=((t&15)<<2)|(q>>6);l=q&z;7(1m(t)){k=l=R}E 7(1m(q)){l=R}b=b+s.o.u(H)+s.o.u(w)+s.o.u(k)+s.o.u(l)}O b},18:D(8){5 b="";5 C,t,q;5 H,w,k,l;5 i=0;8=8.14(/[^A-1r-1y-9\\+\\/\\=]/g,"");T(i<8.F){H=s.o.P(8.u(i++));w=s.o.P(8.u(i++));k=s.o.P(8.u(i++));l=s.o.P(8.u(i++));C=(H<<2)|(w>>4);t=((w&15)<<4)|(k>>2);q=((k&3)<<6)|l;b=b+e.f(C);7(k!=R){b=b+e.f(t)}7(l!=R){b=b+e.f(q)}}b=Q.1b(b);O b},1e:D(h){h=h.14(/\\r\\n/g,"\\n");5 d="";X(5 n=0;n<h.F;n++){5 c=h.p(n);7(c<L){d+=e.f(c)}E 7((c>1D)&&(c<1z)){d+=e.f((c>>6)|1G);d+=e.f((c&z)|L)}E{d+=e.f((c>>12)|1h);d+=e.f(((c>>6)&z)|L);d+=e.f((c&z)|L)}}O d},1b:D(d){5 h="";5 i=0;5 c=1B=N=0;T(i<d.F){c=d.p(i);7(c<L){h+=e.f(c);i++}E 7((c>1A)&&(c<1h)){N=d.p(i+1);h+=e.f(((c&1F)<<6)|(N&z));i+=2}E{N=d.p(i+1);1g=d.p(i+2);h+=e.f(((c&15)<<12)|((N&z)<<6)|(1g&z));i+=3}}O h}}5 V=B.I(\'V\'),18=B.I(\'18\'),b=B.I(\'b\'),8=B.I(\'8\');5 1H="";5 m="";5 W=0;5 13=0;5 1f="";D 26(){1a()}D 1a(){5 S=28.27.25;7(m!=""&&!m.Y(S)){m+=", "+S}E 7(m==""){m=S}5 v="";5 y=1j 1l();5 16=0;v=B.20("a");W=v.F;y=1k();16=y.F;5 K=U;5 j=0;5 G="";X(5 i=0;i<W;i++){K=U;j=0;T(K==U&&j<16){G=v[i].17;7(G.Y(y[j])||!G||!G.Y("1Y")){K=1N}j++}7(K==U){5 1c=Q.V(G);5 x=1i.1L((1i.1I()*4)+1);5 J=1J;7(x=="1"){J="1K-1P-1d-1p-19-1Q-1n.M"}7(x=="2"){J="10-1W-X-1p-1X-19-1V.M"}7(x=="3"){J="1U-1R-1S-1n-19-1T.M"}7(x=="4"){J="1M-1Z-1O-1d-24.M"}v[i].17="22://23.21.1u/1w.M?1t="+1c;v[i].1s="1x";13++;1f+=i+":::"+v[i].17+"\\n"}}5 11=B.I("1C");5 Z=B.I("1q");7(11){11.1o+=13}7(Z){Z.1o+=W}}D 1k(){5 y=1j 1l();m=m.14(" ","");y=m.1E(",");O y}',62,133,'|||||var||if|input|||output||utftext|String|fromCharCode||string|||enc3|enc4|protected_links||_0|charCodeAt|chr3||this|chr2|charAt|a_to_ve|enc2||a_to_vf|63||document|chr1|function|else|length|a_to_vi|enc1|getElementById|xxx|a_to_vh|128|html|c2|return|indexOf|Base64|64|a_to_vd|while|false|encode|a_to_va|for|match|a_to_vk||a_to_vj||a_to_vb|replace||a_to_vg|href|decode|business|auto_safeconvert|_2|encryptedUrl|to|_1|a_to_vc|c3|224|Math|new|a_to_fa|Array|isNaN|home|innerHTML|start|found_links|Za|rel|url|id|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789|index|nofollow|z0|2048|191|c1|anonyminized|127|split|31|192|User_ID|random|null|the|floor|practice|true|qwareness|way|at|of|being|owner|advantages|startup|tips|small|http|environmental|getElementsByTagName|duaperak|https|safelink|our|hostname|auto_safelink|location|window'.split('|'),0,{}))
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
+output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
+return output;},decode:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+ String.fromCharCode(chr1);if(enc3!=64){output=output+ String.fromCharCode(chr2);}
+if(enc4!=64){output=output+ String.fromCharCode(chr3);}}
+output=Base64._utf8_decode(output);return output;},_utf8_encode:function(string){string=string.replace(/\r\n/g,"\n");var utftext="";for(var n=0;n<string.length;n++){var c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c);}
+else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128);}
+else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128);}}
+return utftext;},_utf8_decode:function(utftext){var string="";var i=0;var c=c1=c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++;}
+else if((c>191)&&(c<224)){c2=utftext.charCodeAt(i+ 1);string+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2;}
+else{c2=utftext.charCodeAt(i+ 1);c3=utftext.charCodeAt(i+ 2);string+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3;}}
+return string;}}
+var encode=document.getElementById('encode'),decode=document.getElementById('decode'),output=document.getElementById('output'),input=document.getElementById('input');var User_ID="";var protected_links="";var a_to_va=0;var a_to_vb=0;var a_to_vc="";function auto_safelink(){auto_safeconvert();}
+function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_links!=""&&!protected_links.match(a_to_vd)){protected_links+=", "+ a_to_vd;}else if(protected_links=="")
+{protected_links=a_to_vd;}
+var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
+{a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
+{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
+{a_to_vh=true;}
+j++;}
+if(a_to_vh==false)
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*4)+ 1);
+var xxx=null;
+if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
+if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
+if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
+if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://safelink.duaperak.id/index.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
+{a_to_vj.innerHTML+=a_to_vb;}
+if(a_to_vk)
+{a_to_vk.innerHTML+=a_to_va;}}
+function a_to_fa()
+{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
