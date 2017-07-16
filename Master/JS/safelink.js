f9cb838b617090af4f5f2dfd43d8b24b48d4fe3f
Angga Saputra <masalikzxc@gmail.com>
2017-07-16 17:39:45 +0700

    Update safelink.js

--- Master/JS/safelink.js
+++ Master/JS/safelink.js
@@ -21,10 +21,9 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="http://www.kompisafelinkv2.ga/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="facebook-auto-post-multi-pagesgrup-dan.html"}
+if(x=="2"){xxx="setiva-v1-6-responsive-magazine-blogger.html"}
+if(x=="4"){xxx="domain-net-gratis-2017.html"}a_to_ve[i].href="https://www.duaperak.id/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
