35ccd7e3c99afb9d6ac51041489e7176fb550d3a
Angga Saputra <masalikzxc@gmail.com>
2017-07-16 18:03:04 +0700

    Update safelink.js

--- Master/JS/safelink.js
+++ Master/JS/safelink.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="facebook-auto-post-multi-pagesgrup-dan.html"}
 if(x=="2"){xxx="setiva-v1-6-responsive-magazine-blogger.html"}
-if(x=="4"){xxx="domain-net-gratis-2017.html"}a_to_ve[i].href="https://www.duaperak.id/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="domain-net-gratis-2017.html"}a_to_ve[i].href="https://www.duaperak.id/p/go-url.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
